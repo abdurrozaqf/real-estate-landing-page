0cd6cb102f1dad050a89945844206950e6f53401
abdurrozaqf <rozaqa27@gmail.com>
2024-02-24 09:26:04 +0700

    fix: video components

--- src/modules/home/components/VideoPlayer.tsx
+++ src/modules/home/components/VideoPlayer.tsx
@@ -1,22 +1,41 @@
+import { Button } from "antd";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+
+import { FaPlay } from "react-icons/fa";
 
 interface VideoPlayerProps {}
 
 export default function VideoPlayer({}: VideoPlayerProps) {
   return (
-    <section className="w-full h-auto relative "></section>
-
-    // <section className="w-full h-auto relative">
-    // <div className="w-full bg-[#0D0C1E] h-[360px]" />
-    // <iframe
-    //   src="https://www.youtube.com/embed/mJVuZiK9a6I?si=iENlhqHEgmWdaaai"
-    //   allowFullScreen
-    //   title="Real Estate"
-    //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    //   className="w-[1280px] h-[720px] absolute top-0 bottom-0 left-0 right-0"
-    // />
-    // <div className="w-full h-[360px] bg-[#F4F7FC]" />
-    // </section>
+    <section className="w-full h-auto">
+      <div className="w-full h-[80px] lg:h-[180px] xl:h-[260px] 2xl:h-[360px] bg-[#0D0C1E] relative ">
+        <div className="w-[320px] md:w-[420px] lg:w-[620px] xl:w-[820px] 2xl:w-[1010px] absolute rounded-2xl overflow-hidden top-0 md:-top-12 lg:top-0 right-1/2 translate-x-1/2 ">
+          <Image
+            src="/images/image-9.jpg"
+            alt="image video player"
+            width="0"
+            height="0"
+            sizes="100vw"
+            className="w-full h-auto relative"
+          />
+          <Link
+            href="https://www.youtube.com/embed/mJVuZiK9a6I?si=iENlhqHEgmWdaaai"
+            aria-label="link-video"
+            target="_blank"
+            className="absolute top-1/2 right-1/2 translate-x-10 -translate-y-10 lg:translate-x-16 tra lg:-translate-y-14"
+          >
+            <Button
+              aria-label="play-video"
+              className="h-auto bg-white rounded-full p-6 lg:p-10"
+            >
+              <FaPlay color="blue" size={25} />
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <div className="w-full h-[80px] md:h-[120px] lg:h-[180px] xl:h-[260px] 2xl:h-[360px] bg-[#F4F7FC]" />
+    </section>
   );
 }
