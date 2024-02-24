cbd1aca4c435ea8588eae2768dafa858b2105a3a
abdurrozaqf <rozaqa27@gmail.com>
2024-02-24 10:28:59 +0700

    feat: update metadata home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import { Metadata } from "next";
 import Home from "@/modules/home";
 
 export const metadata: Metadata = {
-  title: `Real Estate`,
+  title: "Real Estate",
+  description: "Helping You Get More For Your Real Estate",
 };
 
 export default function HomePage() {
